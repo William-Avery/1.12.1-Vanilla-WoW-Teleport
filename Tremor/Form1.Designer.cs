7aabe9d34b847b41a63d93bb2b7ea952862533e6
knif3r2 <willaveryvy@gmail.com>
2018-04-28 03:36:23 -0400

    Add project files.

---
+++ Tremor/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Tremor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lv_maps = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.b_teleport = new System.Windows.Forms.Button();
+            this.tb_mapsearch = new System.Windows.Forms.TextBox();
+            this.b_defaultlist = new System.Windows.Forms.Button();
+            this.b_playerlist = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.b_grave = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lv_maps
+            // 
+            this.lv_maps.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lv_maps.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.lv_maps.FullRowSelect = true;
+            this.lv_maps.Location = new System.Drawing.Point(2, 53);
+            this.lv_maps.MultiSelect = false;
+            this.lv_maps.Name = "lv_maps";
+            this.lv_maps.Size = new System.Drawing.Size(209, 220);
+            this.lv_maps.TabIndex = 0;
+            this.lv_maps.UseCompatibleStateImageBehavior = false;
+            this.lv_maps.View = System.Windows.Forms.View.List;
+            this.lv_maps.SelectedIndexChanged += new System.EventHandler(this.lv_maps_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 80;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Y";
+            this.columnHeader2.Width = 50;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "X";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Z";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Map";
+            this.columnHeader5.Width = 40;
+            // 
+            // b_teleport
+            // 
+            this.b_teleport.Location = new System.Drawing.Point(2, 23);
+            this.b_teleport.Name = "b_teleport";
+            this.b_teleport.Size = new System.Drawing.Size(209, 30);
+            this.b_teleport.TabIndex = 1;
+            this.b_teleport.Text = "Teleport";
+            this.b_teleport.UseVisualStyleBackColor = true;
+            this.b_teleport.Click += new System.EventHandler(this.b_teleport_Click);
+            // 
+            // tb_mapsearch
+            // 
+            this.tb_mapsearch.Location = new System.Drawing.Point(2, 0);
+            this.tb_mapsearch.Name = "tb_mapsearch";
+            this.tb_mapsearch.Size = new System.Drawing.Size(209, 22);
+            this.tb_mapsearch.TabIndex = 2;
+            this.tb_mapsearch.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // b_defaultlist
+            // 
+            this.b_defaultlist.Location = new System.Drawing.Point(106, 292);
+            this.b_defaultlist.Name = "b_defaultlist";
+            this.b_defaultlist.Size = new System.Drawing.Size(105, 30);
+            this.b_defaultlist.TabIndex = 3;
+            this.b_defaultlist.Text = "Default Locations";
+            this.b_defaultlist.UseVisualStyleBackColor = true;
+            this.b_defaultlist.Click += new System.EventHandler(this.b_defaultlist_Click);
+            // 
+            // b_playerlist
+            // 
+            this.b_playerlist.Location = new System.Drawing.Point(2, 292);
+            this.b_playerlist.Name = "b_playerlist";
+            this.b_playerlist.Size = new System.Drawing.Size(105, 30);
+            this.b_playerlist.TabIndex = 4;
+            this.b_playerlist.Text = "Player Added";
+            this.b_playerlist.UseVisualStyleBackColor = true;
+            this.b_playerlist.Click += new System.EventHandler(this.b_playerlist_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.b_grave);
+            this.groupBox1.Location = new System.Drawing.Point(217, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(201, 321);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Commands:";
+            // 
+            // b_grave
+            // 
+            this.b_grave.Location = new System.Drawing.Point(6, 21);
+            this.b_grave.Name = "b_grave";
+            this.b_grave.Size = new System.Drawing.Size(189, 23);
+            this.b_grave.TabIndex = 0;
+            this.b_grave.Text = "Teleport Corpse";
+            this.b_grave.UseVisualStyleBackColor = true;
+            this.b_grave.Click += new System.EventHandler(this.b_grave_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 21);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add Current Location";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 323);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.b_playerlist);
+            this.Controls.Add(this.b_defaultlist);
+            this.Controls.Add(this.tb_mapsearch);
+            this.Controls.Add(this.lv_maps);
+            this.Controls.Add(this.b_teleport);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "1.12.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lv_maps;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button b_teleport;
+        private System.Windows.Forms.TextBox tb_mapsearch;
+        private System.Windows.Forms.Button b_defaultlist;
+        private System.Windows.Forms.Button b_playerlist;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button b_grave;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Tremor/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vanilla.CheatEngine;
+
+namespace Tremor
+{
+
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress,
+               [In, Out] byte[] buffer, UInt32 size, out IntPtr lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll")]
+        public static extern int OpenProcess(uint dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess, int lpBaseAddress, byte[] buffer, int size, int lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool WriteProcessMemory(int hProcess, int lpBaseAddress, byte[] buffer, int size, int lpNumberOfBytesWritten);
+
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+
+        Process[] processes = Process.GetProcessesByName("WoW");
+
+        List<Data> Items = new List<Data>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PopulateMapList();
+        }
+
+        private void PopulateMapList()
+        {
+            // get initial data
+            lv_maps.Items.Clear();
+            Items = new List<Data>(){
+            new Data(){ Name="Stormwind", Y="-8913.23", X="554.633", Z="94.7944", Map="0"},
+            new Data(){ Name="Lakeshire", Y="-9282.98", X="-2269.64", Z="70.39", Map="0"},
+            new Data(){ Name="GMIsland", Y="16222.6", X="16265.9", Z="14.2085", Map="1"},
+            /*
+            new Data(){ Name="", Y="", X="", Z="", Map=""}, 
+            */
+        };
+
+            // adding initial data
+            lv_maps.Items.AddRange(Items.Select(c => new ListViewItem(c.Name)).ToArray());
+        }
+
+        private void PopulatePlayerList()
+        {
+            // get initial data
+            lv_maps.Items.Clear();
+            Items = new List<Data>(){
+            new Data(){ Name="1", Y="-8913.23", X="554.633", Z="94.7944", Map="0"},
+            new Data(){ Name="2", Y="-9282.98", X="-2269.64", Z="70.39", Map="0"},
+            new Data(){ Name="3", Y="16222.6", X="16265.9", Z="14.2085", Map="1"},
+            /*
+            new Data(){ Name="", Y="", X="", Z="", Map=""},   
+            */
+        };
+
+            // adding initial data
+            lv_maps.Items.AddRange(Items.Select(c => new ListViewItem(c.Name)).ToArray());
+        }
+
+        private void lv_maps_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            lv_maps.Items.Clear(); // clear list items before adding 
+                                   // filter the items match with search key and add result to list view 
+            lv_maps.Items.AddRange(Items.Where(i => string.IsNullOrEmpty(tb_mapsearch.Text) || i.Name.StartsWith(tb_mapsearch.Text))
+                .Select(c => new ListViewItem(c.Name)).ToArray());
+        }
+
+        private void b_defaultlist_Click(object sender, EventArgs e)
+        {
+            PopulateMapList();
+        }
+
+        private void b_playerlist_Click(object sender, EventArgs e)
+        {
+            PopulatePlayerList();
+        }
+
+        //IntPtr address = memory.GetAddress("\"WoW.exe\"+000AE4EC+68+5C4+120+C+2C8");
+        private void b_teleport_Click(object sender, EventArgs e)
+        {
+            Process[] p = Process.GetProcessesByName("WoW");
+            Console.WriteLine(p[0]);
+            uint DELETE = 0x00010000;
+            uint READ_CONTROL = 0x00020000;
+            uint WRITE_DAC = 0x00040000;
+            uint WRITE_OWNER = 0x00080000;
+            uint SYNCHRONIZE = 0x00100000;
+            uint END = 0xFFF; //if you have Windows XP or Windows Server 2003 you must change this to 0xFFFF
+            uint PROCESS_ALL_ACCESS = (DELETE | READ_CONTROL | WRITE_DAC | WRITE_OWNER | SYNCHRONIZE | END);
+
+            int processHandle = OpenProcess(PROCESS_ALL_ACCESS, false, p[0].Id);
+            int processSize = GetObjectSize("12");
+
+            float x = (float)-4332.891113;
+            float y = (float)-761.9750366;
+            float z = (float)53.79263687;
+
+            using (Memory memory = new Memory(processes[0]))
+            {
+                IntPtr addressy = memory.GetAddress("\"WoW.exe\"+000AE4EC+68+5C4+120+C+2C8");
+                IntPtr addressx = memory.GetAddress("\"WoW.exe\"+0087BCD4+88+28+708+C+2A8");
+                IntPtr addressz = memory.GetAddress("\"WoW.exe\"+000AE4EC+68+690+C+C+318");
+
+                memory.WriteFloat(addressx, x);
+                memory.WriteFloat(addressy, y);
+                memory.WriteFloat(addressz, z);
+            }
+        }
+
+        private static IntPtr GetModuleBaseAddress(string AppName, string ModuleName)
+        {
+            IntPtr BaseAddress = IntPtr.Zero;
+            Process[] myProcess = null;
+            ProcessModule myProcessModule = null;
+
+            myProcess = Process.GetProcessesByName(AppName);
+
+            if (myProcess.Length > 0)
+            {
+                ProcessModuleCollection myProcessModuleCollection;
+
+                try
+                {
+                    myProcessModuleCollection = myProcess[0].Modules;
+                }
+                catch { return IntPtr.Zero; /*Maybe would be ok show the exception after/instead return*/ }
+
+                for (int i = 0; i < myProcessModuleCollection.Count; i++)
+                {
+                    myProcessModule = myProcessModuleCollection[i];
+                    if (myProcessModule.ModuleName.Contains(ModuleName))
+                    {
+                        BaseAddress = myProcessModule.BaseAddress;
+                        break;
+                    }
+                }
+            }
+
+            return BaseAddress;
+        }
+
+        public int GetObjectSize(object TestObject)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+            byte[] Array;
+            bf.Serialize(ms, TestObject);
+            Array = ms.ToArray();
+            return Array.Length;
+        }
+
+        private void b_grave_Click(object sender, EventArgs e)
+        {
+            Process[] p = Process.GetProcessesByName("WoW");
+            Console.WriteLine(p[0]);
+            uint DELETE = 0x00010000;
+            uint READ_CONTROL = 0x00020000;
+            uint WRITE_DAC = 0x00040000;
+            uint WRITE_OWNER = 0x00080000;
+            uint SYNCHRONIZE = 0x00100000;
+            uint END = 0xFFF; //if you have Windows XP or Windows Server 2003 you must change this to 0xFFFF
+            uint PROCESS_ALL_ACCESS = (DELETE | READ_CONTROL | WRITE_DAC | WRITE_OWNER | SYNCHRONIZE | END);
+
+            int processHandle = OpenProcess(PROCESS_ALL_ACCESS, false, p[0].Id);
+            int processSize = GetObjectSize("12");
+
+            float x = (float)-4332.891113;
+            float y = (float)-761.9750366;
+            float z = (float)53.79263687;
+
+            using (Memory memory = new Memory(processes[0]))
+            {
+                // Read Corpse Location
+                IntPtr corpsex = memory.GetAddress("00B4E288");
+                IntPtr corpsey = memory.GetAddress("00B4E284");
+                IntPtr corpsez = memory.GetAddress("00B4E28C");
+                float cor_x = Convert.ToSingle(memory.ReadFloat(corpsex));
+                float cor_y = Convert.ToSingle(memory.ReadFloat(corpsey));
+                float cor_z = Convert.ToSingle(memory.ReadFloat(corpsez));
+
+
+
+                ///Write Memeory
+                IntPtr addressy = memory.GetAddress("\"WoW.exe\"+000AE4EC+68+5C4+120+C+2C8");
+                IntPtr addressx = memory.GetAddress("\"WoW.exe\"+0087BCD4+88+28+708+C+2A8");
+                IntPtr addressz = memory.GetAddress("\"WoW.exe\"+000AE4EC+68+690+C+C+318");
+
+                memory.WriteFloat(addressx, x);
+                memory.WriteFloat(addressy, y);
+                memory.WriteFloat(addressz, z);
+            }
+        }
+    }
+
+
+
+    class Data
+    {
+        public string Name { get; set; }
+        public string Y { get; set; }
+        public string X { get; set; }
+        public string Z { get; set; }
+        public string Map { get; set; }
+    }
+}
